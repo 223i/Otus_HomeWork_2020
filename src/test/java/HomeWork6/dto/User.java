672c30d9afd067b4914211b3e2eeec23c6c3cb39
Ирина <irina@MacBook-Pro-Irina.local>
2020-10-25 00:53:27 +0300

    HomeWork 6 has been done. Test scenarios have been in javadoc described

---
+++ src/test/java/HomeWork6/dto/User.java
@@ -0,0 +1,24 @@
+package HomeWork6.dto;
+
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+@JsonSerialize
+public class User {
+
+    private String email;
+    private String firstName;
+    private Long id;
+    private String lastName;
+    private String password;
+    private String phone;
+    private Long userStatus;
+    private String username;
+}
+
